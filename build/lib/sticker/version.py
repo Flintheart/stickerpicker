38464d17a9822eb6e203282f9ef27ba824306bb1
Ansgar Chmiel <ansgar.chmiel@gmail.com>
2026-02-23 12:16:24 +0100

    update

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '333567f4'
-version = '0.1.0+dev.333567f4'
-linkified_version = '0.1.0+dev.[333567f4](https://github.com/maunium/stickerpicker/commit/333567f481e60443360aa7199d481e1a45b3a523)'
+git_revision = 'edbf68ab'
+version = '0.1.0+dev.edbf68ab'
+linkified_version = '0.1.0+dev.[edbf68ab](https://github.com/maunium/stickerpicker/commit/edbf68ab33549602822db9ed3d51e6c46ce5801c)'
